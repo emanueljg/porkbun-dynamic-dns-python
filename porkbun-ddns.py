f76c8b461c72ec1d5474278c554ae8c7b975a14c
emanueljg <emanueljohnsongodin@gmail.com>
2023-02-15 01:26:57 +0100

    fixup main script

--- porkbun-ddns.py
+++ porkbun-ddns.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import json
 import requests
 import re
@@ -28,26 +29,28 @@ def createRecord():
 	create = json.loads(requests.post(apiConfig["endpoint"] + '/dns/create/'+ rootDomain, data = json.dumps(createObj)).text)
 	return(create)
 
-if len(sys.argv)>2: #at least the config and root domain is specified
-	apiConfig = json.load(open(sys.argv[1])) #load the config file into a variable
-	rootDomain=sys.argv[2].lower()
-		
-	if len(sys.argv)>3 and sys.argv[3]!='-i': #check if a subdomain was specified as the third argument
-		subDomain=sys.argv[3].lower()
-		fqdn=subDomain + "." + rootDomain
-	else:
-		subDomain=''
-		fqdn=rootDomain
+if __name__ == '__main__':
 
-	if len(sys.argv)>4 and sys.argv[3]=='-i': #check if IP is manually specified. There's probably a more-elegant way to do this
-		myIP=sys.argv[4]
-	elif len(sys.argv)>5 and sys.argv[4]=='-i':
-		myIP=sys.argv[5]
-	else:
-		myIP=getMyIP() #otherwise use the detected exterior IP address
-	
-	deleteRecord()
-	print(createRecord()["status"])
-	
-else:
-	print("Porkbun Dynamic DNS client, Python Edition\n\nError: not enough arguments. Examples:\npython porkbun-ddns.py /path/to/config.json example.com\npython porkbun-ddns.py /path/to/config.json example.com www\npython porkbun-ddns.py /path/to/config.json example.com '*'\npython porkbun-ddns.py /path/to/config.json example.com -i 10.0.0.1\n")
+    if len(sys.argv)>2: #at least the config and root domain is specified
+        apiConfig = json.load(open(sys.argv[1])) #load the config file into a variable
+        rootDomain=sys.argv[2].lower()
+            
+        if len(sys.argv)>3 and sys.argv[3]!='-i': #check if a subdomain was specified as the third argument
+            subDomain=sys.argv[3].lower()
+            fqdn=subDomain + "." + rootDomain
+        else:
+            subDomain=''
+            fqdn=rootDomain
+
+        if len(sys.argv)>4 and sys.argv[3]=='-i': #check if IP is manually specified. There's probably a more-elegant way to do this
+            myIP=sys.argv[4]
+        elif len(sys.argv)>5 and sys.argv[4]=='-i':
+            myIP=sys.argv[5]
+        else:
+            myIP=getMyIP() #otherwise use the detected exterior IP address
+        
+        deleteRecord()
+        print(createRecord()["status"])
+        
+    else:
+        print("Porkbun Dynamic DNS client, Python Edition\n\nError: not enough arguments. Examples:\npython porkbun-ddns.py /path/to/config.json example.com\npython porkbun-ddns.py /path/to/config.json example.com www\npython porkbun-ddns.py /path/to/config.json example.com '*'\npython porkbun-ddns.py /path/to/config.json example.com -i 10.0.0.1\n")
